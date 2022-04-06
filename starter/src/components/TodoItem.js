30fa7c43d3d0477466f93c0e07d558a62808661a
ZorsalG <alejandrofal98@gmail.com>
2022-04-06 14:13:10 +0200

    Todo implemented

--- starter/src/components/TodoItem.js
+++ starter/src/components/TodoItem.js
@@ -1,14 +1,26 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { toggleCompleteAsync, deleteTodo } from '../redux/todoSlice';
 
 const TodoItem = ({ id, title, completed }) => {
+	const dispatch = useDispatch();
+
+	const handleCompleteClick = () => {
+		dispatch(toggleCompleteAsync({ id: id, completed: !completed }))
+	}
+
+	const handleDeleteCLick = () => {
+		dispatch(deleteTodo({id: id}));
+	};
+
 	return (
 		<li className={`list-group-item ${completed && 'list-group-item-success'}`}>
 			<div className='d-flex justify-content-between'>
 				<span className='d-flex align-items-center'>
-					<input type='checkbox' className='mr-3' checked={completed}></input>
+					<input type='checkbox' className='mr-3' checked={completed} onChange={handleCompleteClick}></input>
 					{title}
 				</span>
-				<button className='btn btn-danger'>Delete</button>
+				<button onClick={handleDeleteCLick} className='btn btn-danger'>Delete</button>
 			</div>
 		</li>
 	);
